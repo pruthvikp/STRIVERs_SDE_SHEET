60eb360a1662c1836c4823355d61a9e89e3aeada
Pruthvi K P <117765336+pruthvikp@users.noreply.github.com>
2024-07-24 10:44:35 +0530

    Update 48_RotateImage.py

--- Day2_Arrays/48_RotateImage.py
+++ Day2_Arrays/48_RotateImage.py
@@ -19,19 +19,36 @@ n == matrix.length == matrix[i].length
 
 '''
 
+# Approach 1:
+# Take another dummy matrix of n*n, and then take the first row of the matrix and put it in the last column of the dummy matrix, take the second row of the matrix, and put it in the second last column of the matrix and so.
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        rotated = [[0 for _ in range(n)] for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                rotated[j][n-i-1] = matrix[i][j]
+        matrix = [row[:] for row in rotated]
+# Note: This approach won't work in leetcode because the modified matrix inside the function is a local variable, and it does not affect the original matrix object that was passed into the function. 
+
+# Approach 2: Rotating the Matrix in Place
+# Transpose the matrix. 
+# Reverse each row of the matrix.
+
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
         n=len(matrix)
+
         for i in range(n):
-            for j in range(i):
+            for j in range(i,n):
                 matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
-        
-        for i in range(n):
-            left,right=0,n-1
-            while left<right:
-                matrix[i][left],matrix[i][right]=matrix[i][right],matrix[i][left]
-                left+=1
-                right-=1
+
+        for row in matrix:
+            row.reverse()
