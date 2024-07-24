e44d0d5ae7907c68050a8e7a36e599cd878c3b6e
Pruthvi K P <117765336+pruthvikp@users.noreply.github.com>
2024-07-24 10:46:42 +0530

    Update 48_RotateImage.py

--- Day2_Arrays/48_RotateImage.py
+++ Day2_Arrays/48_RotateImage.py
@@ -46,8 +46,8 @@ class Solution:
         """
         n=len(matrix)
 
-        for i in range(n):
-            for j in range(i,n):
+        for i in range(n-1):
+            for j in range(i+1,n):
                 matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
 
         for row in matrix:
