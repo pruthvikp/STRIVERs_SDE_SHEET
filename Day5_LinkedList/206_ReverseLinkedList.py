304acb5454422a46b436b98ba0651d3931422730
Pruthvi K P <117765336+pruthvikp@users.noreply.github.com>
2024-08-19 08:59:06 +0530

    Update 206_ReverseLinkedList.py

--- Day5_LinkedList/206_ReverseLinkedList.py
+++ Day5_LinkedList/206_ReverseLinkedList.py
@@ -18,9 +18,9 @@ The number of nodes in the list is the range [0, 5000].
 Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
 '''
 
-The approach iteratively reverses the linked list by traversing it and adjusting the `next` pointers of each node to point to the previous node. 
-A `dummy` node tracks the reversed list's head, and `temp` temporarily stores the next node in the original list. 
-Finally, the `head` is updated to point to the new head of the reversed list, and the reversed list is returned.
+# The approach iteratively reverses the linked list by traversing it and adjusting the `next` pointers of each node to point to the previous node. 
+# A `dummy` node tracks the reversed list's head, and `temp` temporarily stores the next node in the original list. 
+# Finally, the `head` is updated to point to the new head of the reversed list, and the reversed list is returned.
 
 # Definition for singly-linked list.
 # class ListNode:
