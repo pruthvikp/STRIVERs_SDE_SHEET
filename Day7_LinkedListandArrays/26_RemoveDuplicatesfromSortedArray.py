71bdb7a4fb003bc27f99e79a21739c18d7be1d1c
Pruthvi K P <117765336+pruthvikp@users.noreply.github.com>
2024-08-26 09:50:55 +0530

    Update 26_RemoveDuplicatesfromSortedArray.py

--- Day7_LinkedListandArrays/26_RemoveDuplicatesfromSortedArray.py
+++ Day7_LinkedListandArrays/26_RemoveDuplicatesfromSortedArray.py
@@ -1,1 +1,78 @@
 '''
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+
+Custom Judge:
+The judge will test your solution with the following code:
+int[] nums = [...]; // Input array
+int[] expectedNums = [...]; // The expected answer with correct length
+
+int k = removeDuplicates(nums); // Calls your implementation
+
+assert k == expectedNums.length;
+for (int i = 0; i < k; i++) {
+    assert nums[i] == expectedNums[i];
+}
+If all assertions pass, then your solution will be accepted.
+
+Example 1:
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+ 
+Constraints:
+1 <= nums.length <= 3 * 104
+-100 <= nums[i] <= 100
+nums is sorted in non-decreasing order.
+'''
+
+# Approach 1:
+# The approach iterates through the list, counting consecutive duplicate elements. 
+# It places each unique element in a new position in the list, effectively removing duplicates. 
+# Finally, it returns the length of the modified list without duplicates.
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n=len(nums)
+        i=0
+        j=-1
+        while i<n:
+            rv=nums[i]
+            rl=1
+            while i+rl<n and nums[i+rl]==rv:
+                rl+=1
+            j+=1
+            nums[j]=rv
+            i+=rl
+        return j+1
+
+
+# Approach 2: 
+# This approach uses a two-pointer technique to remove duplicates in-place. 
+# It iterates through the list, comparing each element with the previous one. 
+# When a unique element is found, it is moved to the front of the list, and a pointer `j` is incremented. 
+# Finally, the method returns `j`, representing the length of the modified list with unique elements.
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        j = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[j] = nums[i]
+                j += 1
+        return j
